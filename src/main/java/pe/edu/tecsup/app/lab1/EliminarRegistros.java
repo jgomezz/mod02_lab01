7feeb896731811eadc7a0850baffbd2c74c41eb8
jgomezz <jgomezz@gmail.com>
2024-10-27 09:35:50 -0500

    update

---
+++ src/main/java/pe/edu/tecsup/app/lab1/EliminarRegistros.java
@@ -0,0 +1,49 @@
+package pe.edu.tecsup.lab1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EliminarRegistros extends JDBCBase {
+
+	public static void main(String[] args) {
+		
+		try {
+			// Conectarme a la BD
+			Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+			// Define la sentencia SQL
+			String sql = 
+					"""
+						DELETE FROM categorias 
+						WHERE id = ?
+						
+					""";	
+			int idBorrar = 14; // 14 es el ID de Lentes            
+			PreparedStatement stmt = con.prepareStatement(sql);              
+			stmt.setInt(1, idBorrar); 
+		
+			// Ejecutar el borrado
+			int estado = stmt.executeUpdate(); 
+		
+			if (estado != 1)
+				throw new SQLException("No se pudo eliminar");
+
+			System.out.printf("Se elimino la categoria con id = %d \n", idBorrar);
+			
+						
+			stmt.close();
+			
+			// Cerrar conexion a la BD
+			con.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
