382771025cc91776977340e5217818318418019e
jgomezz <jgomezz@gmail.com>
2024-10-27 09:14:17 -0500

    update

--- src/main/java/pe/edu/tecsup/app/lab1/InsertarRegistros.java
+++ src/main/java/pe/edu/tecsup/app/lab1/InsertarRegistros.java
@@ -1,4 +1,4 @@
-package pe.edu.tecsup.lab1;
+package pe.edu.tecsup.app.lab1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,42 +8,41 @@ import java.sql.SQLException;
 public class InsertarRegistros extends JDBCBase {
 
 	public static void main(String[] args) {
-		
+
 		try {
 			// Conectarme a la BD
 			Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
 			// Realizar la insercion
-			String sql = 
+			String sql =
 					"""
-						INSERT INTO productos (nombre, descripcion, precio) 
-						VALUES (?, ?, ?);
-					""";	
-			
-			PreparedStatement stmt = con.prepareStatement(sql);              
-			
-			stmt.setString(1, "lentes");               
-			stmt.setString(2, "Lentes de realidad virtual");             
-			stmt.setInt(3, 2000);              
-		
+						INSERT INTO categorias (nombre, descripcion) 
+						VALUES (?, ?);
+					""";
+
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			stmt.setString(1, "Lentes");
+			stmt.setString(2, "Lentes de realidad virtual");
+
 			// Ejecutar la insercion
-			int estado = stmt.executeUpdate(); 
-			
+			int estado = stmt.executeUpdate();
+
 			// Analizar el resultado
 			if (estado != 1)
 				throw new SQLException("No se pudo insertar");
-			
-						
+
+
 			stmt.close();
-			
+
 			// Cerrar conexion a la BD
 			con.close();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 
 	}
 
