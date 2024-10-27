38a50483a4af462c7495db8250f0342a8f873c85
jgomezz <jgomezz@gmail.com>
2024-10-27 09:29:59 -0500

    update

--- src/main/java/pe/edu/tecsup/app/lab1/ActualizarRegistros.java
+++ src/main/java/pe/edu/tecsup/app/lab1/ActualizarRegistros.java
@@ -1,4 +1,4 @@
-package pe.edu.tecsup.lab1;
+package pe.edu.tecsup.app.lab1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -8,42 +8,43 @@ import java.sql.SQLException;
 public class ActualizarRegistros extends JDBCBase {
 
 	public static void main(String[] args) {
-		
+
 		try {
 			// Conectarme a la BD
 			Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
-			// Realizar la insercion
-			String sql = 
+			// Define la sentencia SQL
+			String sql =
 					"""
-						UPDATE categorias SET nombre = ? 
+						UPDATE categorias 
+						SET nombre = ? 
 						WHERE id = ?
 						
-					""";	
-			
-			PreparedStatement stmt = con.prepareStatement(sql);              
-			
-			stmt.setString(1, "Lentes Cambiados");               
-			stmt.setInt(2, 14);             
-		
-			// Ejecutar la insercion
-			int estado = stmt.executeUpdate(); 
-			
+					""";
+
+			PreparedStatement stmt = con.prepareStatement(sql);
+
+			stmt.setString(1, "Lentes Cambiados");
+			stmt.setInt(2, 14); // 14 es el ID de Lentes
+
+			// Ejecutar la actualizacion
+			int estado = stmt.executeUpdate();
+
 			// Analizar el resultado
 			if (estado != 1)
 				throw new SQLException("No se pudo insertar");
-			
-						
+
+
 			stmt.close();
-			
+
 			// Cerrar conexion a la BD
 			con.close();
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 
 	}
 
